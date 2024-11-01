371b4eb2696507453e15601a82d21d2c719785f6
Adam Gough <adam_gough@brown.edu>
2024-10-31 22:37:21 -0700

    started home page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+
+      </head>
+      <body class="body">
+        <div id="grain" className="title-container" style={{ position: "relative", overflow: "hidden" }}></div>
+        <div class="navbar w-nav">
+          <div class="nav-container w-container">
+            <a class="logo w-nav-brand w--current">
+              <div class="name-text">
+                <h1>Evervest FP</h1>
+              </div>
+            </a>
+            <nav role="navigation" class="nav-menu w-nav-menu">
+              <div class="nav-div-left">
+                <h2 href= "google.com" class="nav-text-link">Our Story</h2>
+                <h2 href= "google.com" class="nav-text-link">Our Team</h2>
+                <h2 href= "google.com" class="nav-text-link">Contact Us</h2>
+              </div>
+            </nav>
+          </div>
+          <div class="w-nav-overlay"></div>
+        </div>
+        <section class="hero-wrapper">
+          <div class="hero-div">
+            <p class="hero-text">
+                Evervest Financial Planning
+            </p>
+            <a class="word">
+              Empower your future sustainably.
+            </a>
+          </div>
+        </section>
+        <div class="email-pass-container">
+
+        </div>
+        <div class="sign-log-in-container">
+
+        </div>
+        <div class="socials-container">
+
+        </div>
+    </body>
     </div>
   );
 }
