f31ace0b4a280365b6a677b6765b83e92202a71d
Adam Gough <adam_gough@brown.edu>
2024-11-11 18:57:16 -0800

    bigger commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            <h1>Hello, Adam Gough</h1>
+            {/* Add your dashboard content here */}
+        </div>
+    );
+}
+
+export default Dashboard;
